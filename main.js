3bc90aa113e09900781f123e124b38c73061d696
souphia <sophia.machulek@escola.pr.gov.br>
2026-05-04 11:56:43 -0300

    sffd

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const botoes = document.querySelectorAll("botao");
+const botoes = document.querySelectorAll(".aba-conteudo");
 console.log(botoes)
 for (let i=0;i < botoes.length; i++){
     botoes[i].onclick = function(){
@@ -6,10 +6,12 @@ for (let i=0;i < botoes.length; i++){
 
         for(let j=0; j<botoes.length; j++){
             botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo")
         }
 
 
         botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo")
     }
 
 }
